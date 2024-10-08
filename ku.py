66ea5d9de483733e451b4f2517a5580f48d2ea81
Anonymous <kulycky@gmail.com>
2024-10-08 17:30:08 +0300

    ewfef

---
+++ ku.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df = pd.read_csv("GoogleApps.csv")
+temp = df["Content Rating"].value_counts()
+everyone = temp["Teen"]
+everyone_10 = temp["Everyone 10+"]
+print(everyone/everyone_10)
+temp = df.groupby(by = "Category")["Size"].agg(["min", "max"])
+print(df[df["Type"] == "Paid"].groupby(by = "Contend Rating")["Price"].agg(["min", "mean", "max"]))
+temp = df.pivot_table(index = "Content Rating", columns = "Category", values = "Reviews", aggfunc = "max")
+print(temp["EDUCATION", "FAMILY", "GAME"])
