6c5158d24d0283ceae9d4d70a3a51542cac01115
Anonymous <kulycky@gmail.com>
2024-11-05 17:23:38 +0200

    kuhgjk

---
+++ POO.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv("IMDB-Movie-Data.csv")
+
+
+print(df["Year"].max())
--- ku.py
+++ ku.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv("GoogleApps.csv")
+df = pd.read_csv("101010.csv")
 temp = df["Content Rating"].value_counts()
 everyone = temp["Teen"]
 everyone_10 = temp["Everyone 10+"]
@@ -8,3 +8,5 @@ temp = df.groupby(by = "Category")["Size"].agg(["min", "max"])
 print(df[df["Type"] == "Paid"].groupby(by = "Contend Rating")["Price"].agg(["min", "mean", "max"]))
 temp = df.pivot_table(index = "Content Rating", columns = "Category", values = "Reviews", aggfunc = "max")
 print(temp["EDUCATION", "FAMILY", "GAME"])
+
+temp = df(by = "Rating").agg(["max"])
