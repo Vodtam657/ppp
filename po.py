a342fedbee2f581575d6117f8c5a0299d2e2b20f
vladislav602 <kulycky@gmail.com>
2024-11-19 16:59:04 +0200

    ghhj

--- po.py
+++ po.py
@@ -1,12 +1,16 @@
 import pandas as pd
-df = pd.read_csv("IMDB-Movie-Data.csv")
+df = pd.read_csv("train.csv")
 
-print(df["Rating"].mean())
-
-
+print(df["result"].mean())
 
 
 
+X = df.drop("result", axis = 1)
+y = df["result"]
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
 
 
 
